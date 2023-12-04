c2411d7d3faa64a7eab692410a31926206c6f219
shubhamkcs <shubham.varneshiya@kcsitglobal.com>
2023-12-04 14:27:30 +0530

    update wokflow

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Express API!' });
 });
 
+app.get('/test', (req, res) => {
+  res.status(200).json({ message: 'test reached successfully!' });
+})
+
 // You can define more routes and middleware here
 app.listen(8000,()=>{
     console.log('server is running');
