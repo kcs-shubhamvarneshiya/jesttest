f8dc729f8f0772adee1849d7842d4d0908c642a7
shubhamkcs <shubham.varneshiya@kcsitglobal.com>
2023-12-08 10:57:51 +0530

    update file

--- src/index.js
+++ src/index.js
@@ -10,8 +10,4 @@ app.get('/test', (req, res) => {
   res.status(200).json({ message: 'test reached successfully!' });
 })
 
-// You can define more routes and middleware here
-app.listen(8000,()=>{
-    console.log('server is running');
-})
 module.exports = app;
--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,26 @@
 const request = require('supertest');
 const app = require('../src/index');
 
+
+let server;
+
+beforeAll((done) => {
+  server = app.listen(8000, () => {
+    console.log('Server is running');
+    done();
+  });
+});
+
+afterAll((done) => {
+  server.close(done);
+});
+
+
 describe('Test the root path', () => {
   test('It should respond with a welcome message', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
     expect(response.body.message).toBe('Welcome to the Express API!');
   });
-  
+
 });
